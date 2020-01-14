6773f925e468ca5d95f4f9607a7e12c5ada292d8
Distintiva Solutions <jose@distintiva.com>
2020-01-14 21:19:51 +0100

    Update mbit_display.cpp

--- src/mbit_display.cpp
+++ src/mbit_display.cpp
@@ -1,21 +1,7 @@
 /*
-  mbit_display.cpp - LED Screen library for micro:bit
-  (for "Arduino Core for Nordic Semiconductor nRF5 based boards")
-  Copyright (c) 2017 Hideaki Tominaga. All rights reserved.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+Some code od mbit_display is based on https://github.com/ht-deko/microbit_Screen
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "mbit_display.h"
